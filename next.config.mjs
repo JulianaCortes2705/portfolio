07e45a524be61cbe2c63da2d3093628c27667849
JulianaCortes2705 <156023298+JulianaCortes2705@users.noreply.github.com>
2026-04-13 06:56:13 +0000

    Fix GitHub Pages image loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,12 @@
-// get env variable
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const env = process.env.NODE_ENV || 'development';
-
 /** @type {import('next').NextConfig} */
-const nextConfigProd = {
+const nextConfig = {
   output: 'export',
   basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   images: {
-    loader: 'custom',
-    loaderFile: './src/lib/image.loader.js',
+    unoptimized: true,
   },
+  trailingSlash: true,
 };
-
-/** @type {import('next').NextConfig} */
-const nextConfigDev = {};
-
-const nextConfig = env === 'development' ? nextConfigDev : nextConfigProd;
 
 export default nextConfig;
